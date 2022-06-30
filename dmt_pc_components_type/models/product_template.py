d1ffda26816f39947e776d16f414ae038ffd433f
inteligosrodrigo <desarrollador03@inteligos.gt>
2022-06-30 12:12:35 -0600

    feature(back-end):ADD dmt_pc_components_type
    Se añade el modulo de componentes de pc para DMT

--- dmt_pc_components_type/models/product_template.py
+++ dmt_pc_components_type/models/product_template.py
@@ -51,3 +51,43 @@ class ProductTemplateInherit(Model):
                               ('128gb', '128GB'),
                               ], string='Capacidad de RAM')
 
+    monitor_size = Selection([('17pulg', '17 Pulg'),
+                              ('19pulg', '19 Pulg'),
+                              ('20pulg', '20 Pulg'),
+                              ('21pulg', '21 Pulg'),
+                              ('22pulg', '22 Pulg'),
+                              ('23pulg', '23 Pulg'),
+                              ('24pulg', '24 Pulg'),
+                              ('25pulg', '25 Pulg'),
+                              ('26pulg', '26 Pulg'),
+                              ('27pulg', '27 Pulg'),
+                              ], string='Tamaño de monitor')
+
+    monitor_type = Selection([('square', 'Cuadrado'),
+                              ('wide', 'Wide'),
+                              ], string='Tipo de Monitor')
+
+    mouse = Selection([('new', 'N'),
+                       ('reusable', 'R'),
+                       ], string='Mouse')
+
+    keyboard = Selection([('new', 'N'),
+                          ('reusable', 'R'),
+                          ], string='Teclado')
+
+    video_cable = Selection([('new', 'N'),
+                             ('reusable', 'R'),
+                             ], string='Cable de Video')
+
+    power_cable = Selection([('new', 'N'),
+                             ('reusable', 'R'),
+                             ], string='Cable de Poder')
+
+    speakers = Selection([('new', 'N'),
+                          ], string='Bocina')
+
+    printer = Selection([('new', 'N'),
+                         ], string='Impresora')
+
+    ups = Selection([('new', 'N'),
+                     ], string='UPS')
